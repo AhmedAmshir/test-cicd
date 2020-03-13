a59e77662adccbba16d9a8c4ec59d839429bf243
Ahmed Maher Amshir <ahmd.amshir@gmail.com>
2020-03-13 22:52:47 +0200

    change test numbers

--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -23,8 +23,8 @@ class CalculatorTest extends TestCase
     public function testDividTwoNumber()
     {
         $calculators = new Calculators();
-        $num1 = 10;
-        $num2 = 2;
+        $num1 = 15;
+        $num2 = 3;
         $this->assertEquals(5, $calculators->divid($num1, $num2));
     }
 
