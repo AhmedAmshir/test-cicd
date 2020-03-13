d417668e8d75e0e1fd65ed8966e7785835dc6c41
Ahmed Maher Amshir <ahmd.amshir@gmail.com>
2020-03-13 22:31:14 +0200

    add new method to divid 2 numbers

--- app/Managers/Calculators.php
+++ app/Managers/Calculators.php
@@ -7,4 +7,8 @@ class Calculators {
     public function sum($num1, $num2) {
         return $num1 + $num2;
     }
+
+    public function divid($num1, $num2) {
+        return $num1 / $num2;
+    }
 }
--- tests/Unit/CalculatorTest.php
+++ tests/Unit/CalculatorTest.php
@@ -19,4 +19,13 @@ class CalculatorTest extends TestCase
         $num2 = 10;
         $this->assertEquals(15, $calculators->sum($num1, $num2));
     }
+
+    public function testDividTwoNumber()
+    {
+        $calculators = new Calculators();
+        $num1 = 10;
+        $num2 = 2;
+        $this->assertEquals(5, $calculators->divid($num1, $num2));
+    }
+
 }
